1d4e59e2b1898097487d52de92df5fabb5a66932
gopalkushwah <gopalkushwah15101999@gmail.com>
2023-09-12 10:14:08 +0530

    final commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
 import './App.css';
-
+import { useState } from 'react';
 function App() {
+  const [hori, setHori] = useState(10);
+  const [vert, setVert] = useState(10);
+  const [blur, setBlur] = useState(10);
+  const [color, setColor] = useState("black");
+  const [checkOn, setCheckOn] = useState(false);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="controls">
+        <label htmlFor="">Horizontal length</label>
+        <input type="range" min="-200" max="200" value={hori} onChange={(e) => setHori(e.target.value)} />
+        <label htmlFor="">Vertical length</label>
+        <input type="range" min="-200" max="200" value={vert} onChange={(e) => setVert(e.target.value)} />
+        <label htmlFor="">Blur</label>
+        <input type="range" min="0" max="200" value={blur} onChange={(e) => setBlur(e.target.value)} />
+        <label htmlFor="">color</label>
+        <input type="color" value={color} onChange={(e) => setColor(e.target.value)} />
+        <div className="switch">
+          <label>
+            Inset
+            <input type="checkbox" checked={checkOn} onChange={(e) => setCheckOn(!checkOn)} />
+            <span className="lever"></span>
+            Outset
+          </label>
+        </div>
+      </div>
+      <div className="output">
+        <div className="box" style={{
+          boxShadow: `${checkOn ? "inset" : ""} ${hori}px ${vert}px ${blur}px ${color}`
+        }}>
+          <p>box-shadow:{hori}px {vert}px {blur}px {color}</p>
+        </div>
+      </div>
     </div>
   );
 }
